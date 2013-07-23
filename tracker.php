fca76b5fbb8681db50729fcfc3026de6daea9b26
Colin Zeidler <colin.zeidler@magorcorp.com>
2013-07-23 15:54:13 -0400

    testing JIRA searches

--- tracker.php
+++ tracker.php
@@ -6,5 +6,6 @@ $inprog = $_GET['inprog'];
 $fromdate = $_GET['fromdate'];
 $todate = $_GET['todate'];
 
-echo `/home/videowall/python2.7-env/bin/python2.7 tracker.py $case $plan $complete $inprog $fromedate $todate`
+echo "$case $plan $complete $inprog $fromdate $todate<br>";
+echo `/home/videowall/python2.7-env/bin/python2.7 tracker.py $case $plan $complete $inprog $fromedate $todate`;
 ?>
--- tracker.py
+++ tracker.py
@@ -4,13 +4,37 @@ url = "https://magorcorp.atlassian.net/rest/api/2/search"
 user = "auto.verification"
 passw = "video123"
 
-case = sys.argv[0]
-plan = sys.argv[1]
+case = sys.argv[1]
+plan = sys.argv[2]
 
-complete = sys.argv[2]
-inprog = sys.argv[3]
+complete = sys.argv[3]
+inprog = sys.argv[4]
 
-fromdate = sys.argv[4]
-todate = sys.argv[5]
+fromdate = sys.argv[5]
+todate = sys.argv[6]
 
+query = "project=Verification&created>=\"-{}d\"&created<=\"-{}d\"".format(fromdate, todate)
 
+if case == 'true':
+    if plan == 'true':
+        query = query + "&(type=\"Test Case\"+OR+type=\"Test Plan\")"
+    else:
+        query = query + "&type=\"Test Case\""
+elif plan == 'true':
+    query = query + "&type=\"Test Plan\""
+
+if inprog == 'true':
+    if complete == 'false':
+        query = query + "&status=\"In Progress\""
+elif complete == 'true':
+    query = query + "&status!=\"In Progress\""
+
+query = query + "+order+by+created"
+print query
+
+r = requests.get("{}?jql={}".format(url, query), auth = (user, passw))
+
+print r
+
+for issue in r.json()['issues']:
+    print issue['key']
